7294d2322f6fe184f4380a31328e647e1e6242a5
Bruce Sheplan <bruce@sheplan.com>
2026-01-16 18:06:33 -0600

    Add admin page entry point

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
       input: {
         main: 'index.html', // Your main application entry point
         beta: 'beta.html', // Your new beta page entry point
+        admin: 'admin.html', // Admin Page
       },
     },
     sourceMap: true, // Enable source maps for easier debugging
