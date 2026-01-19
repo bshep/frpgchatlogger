15d5b9d7f0e3b8db5ed238e9bb6beda029fe9f07
Bruce Sheplan <bruce@sheplan.com>
2026-01-19 15:59:06 -0600

    Vite Bug Fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         main: 'index.html', // Your main application entry point
         beta: 'beta.html', // Your new beta page entry point
         admin: 'admin.html', // Admin Page
-        admin: 'analysis.html', // Admin Page
+        analysis: 'analysis.html', // Analysis Page
       },
     },
     sourceMap: true, // Enable source maps for easier debugging
